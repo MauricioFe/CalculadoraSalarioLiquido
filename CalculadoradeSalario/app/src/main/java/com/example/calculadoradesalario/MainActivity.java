4cc25e3eb7dd5dc848474ea23bc3c74ec6b71ce4
Maurício Lacerda <mauricio.lacerdaml@gmail.com>
2020-10-06 11:31:01 -0300

    Criando um draweble para inputs brancos

--- CalculadoradeSalario/app/src/main/java/com/example/calculadoradesalario/MainActivity.java
+++ CalculadoradeSalario/app/src/main/java/com/example/calculadoradesalario/MainActivity.java
@@ -3,12 +3,19 @@ package com.example.calculadoradesalario;
 import androidx.appcompat.app.AppCompatActivity;
 
 import android.os.Bundle;
+import android.widget.EditText;
 
 public class MainActivity extends AppCompatActivity {
+    EditText edtSalario;
+    EditText edtDependentes;
+    EditText edtOutros;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
+        edtSalario = findViewById(R.id.main_edt_salario_bruto);
+        edtDependentes = findViewById(R.id.main_edt_dependentes);
+        edtOutros = findViewById(R.id.main_edt_outros);
     }
 }
